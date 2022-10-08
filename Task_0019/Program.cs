edb94b62b4d41405d92c9344f3d1088be9a159f3
MrTihon <tihonsomov@mail.ru>
2022-10-08 14:42:26 +0300

    add Task_0019

--- Task_0019/Program.cs
+++ Task_0019/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// else if (num <= 10000 && num >= 99999)
+
+Console.WriteLine("Введите целое пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10000 && num <= 99999)
+{
+    Console.WriteLine((num / 10000 == num % 10) && ((num / 1000) % 10 == (num % 100) / 10) ? "да" : "нет");
+}
+else Console.WriteLine("не пятизначное число");
